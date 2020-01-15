6595fd588c454857c84a247d7bf41f23c4688519
Nick Boober <nboober@terpmail.umd.edu>
2020-01-15 12:54:20 -0500

    added components for navbar, about me, and my projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+import AboutMe from './components/AboutMe'
+import NavBar from './components/NavBar'
+import Projects from './components/Projects'
 
 class App extends React.Component{
   render(){
     return(
       <div>
+
+        <NavBar/>
+        
         <Switch>
+
+          <AboutMe/>
+
+          <Projects/>
 
         </Switch>
       </div>
