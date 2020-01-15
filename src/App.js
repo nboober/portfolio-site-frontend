4845b61f4e28b9d411ec22a3f8b57b09f47ac555
Nick Boober <nboober@terpmail.umd.edu>
2020-01-15 12:39:43 -0500

    installed react router dom into my project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component{
   render(){
     return(
       <div>
-        
+        <Switch>
+
+        </Switch>
       </div>
     )
   }
