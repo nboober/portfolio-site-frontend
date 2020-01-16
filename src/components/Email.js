27af199f14cd262e7c24aa36c6153abaa724642a
Nick Boober <nboober@terpmail.umd.edu>
2020-01-16 16:49:21 -0500

    successfully incorporated email functionality into my portfolio site

--- src/components/Email.js
+++ src/components/Email.js
@@ -19,10 +19,12 @@ class Email extends React.Component{
       }
 
     
-      handleSubmit = () => {
+      handleSubmit = (event) => {
+        event.preventDefault();
+
         const templateId = 'basic';
     
-        this.sendFeedback(templateId, {message: this.state.feedback, user_name: this.state.name, user_email: this.state.email})
+        this.sendFeedback(templateId, {message: this.state.feedback, name: this.state.name, email: this.state.email})
       }
     
       sendFeedback = (templateId, variables) => {
@@ -38,16 +40,23 @@ class Email extends React.Component{
     
       render() {
         return (
-          <form className="test-mailing">
-            <h4>You can also send me an email directly from here</h4>
+          <form className="test-mailing" onSubmit={this.handleSubmit}>
+
+            <h5>You can also send me an email directly from here</h5>
+
             <div>
                 <label htmlFor="name">Name</label>
-                <input name="user_name" type="text" id="name" onChange={this.nameChange} required value={this.state.name}/>
+                <input name="user_name" type="text" id="name" onChange={this.nameChange} required/>
             </div>
+
             <div>
                 <label htmlFor="email">Email</label>
-                <input name="user_email" type="text" id="email" onChange={this.emailChange} required value={this.state.email}/>
+                <input name="user_email" type="text" id="email" onChange={this.emailChange} required/>
             </div>
+
+            <label htmlFor="message">
+                Message
+            </label>
             <div>
               <textarea
                 id="message"
@@ -55,11 +64,11 @@ class Email extends React.Component{
                 onChange={this.messageChange}
                 placeholder="Put your message here"
                 required
-                value={this.state.feedback}
                 style={{width: '50%', height: '150px'}}
               />
             </div>
-            <input type="button" value="Submit" className="btn btn--submit" onClick={this.handleSubmit} />
+
+            <input type="submit" value="Submit" className="btn btn--submit" />
           </form>
         )
       }
