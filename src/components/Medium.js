cde432563e18ff26aff965a2c249d8c523cd383f
Nick Boober <nboober@terpmail.umd.edu>
2020-01-17 14:51:23 -0500

    successfully connected the medium api url from the back end to the medium component

--- src/components/Medium.js
+++ src/components/Medium.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 class Medium extends React.Component{
+
+    componentDidMount = () =>{
+        fetch('http://localhost:3000/mediumposts')
+        .then(response => response.json())
+        .then(mediumItems => console.log(mediumItems))
+    }
+
     render(){
         return (
             <div>
