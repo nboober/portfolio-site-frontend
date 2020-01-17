dd458a044bb619bb37761c2e604fe726525dbae1
Nick Boober <nboober@terpmail.umd.edu>
2020-01-17 16:15:36 -0500

    updated the medium card css

--- src/components/MediumCards.js
+++ src/components/MediumCards.js
@@ -4,11 +4,11 @@ class MediumCards extends React.Component{
     render(){
         return(
             <div className="medium-cards-container">
-                <a href={this.props.post.link} target="_blank" className="card bg-dark text-white" >
+                <a href={this.props.post.link} target="_blank" rel="noopener noreferrer" className="card bg-dark text-white" >
                     <img className="card-img" src={this.props.post.thumbnail} alt="medium post thumbnail"/>
                     <div className="card-img-overlay" >
-                    <h5 className="card-title card-text-shadow">{this.props.post.title}</h5>
-                    <p className="card-text card-text-shadow">Posted on: {this.props.post.pubDate}</p>
+                    <h5 className="card-title card-text">{this.props.post.title}</h5>
+                    <p className="card-text card-text">Posted on: {this.props.post.pubDate}</p>
                     </div>
                 </a>
             </div>
