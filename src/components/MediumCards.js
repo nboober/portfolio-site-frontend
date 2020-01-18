5a606cd993d508e2d75037665cb1a991f738b232
Nick Boober <nboober@terpmail.umd.edu>
2020-01-18 12:10:58 -0500

    adjusted the styling for my blog post cards

--- src/components/MediumCards.js
+++ src/components/MediumCards.js
@@ -5,8 +5,8 @@ class MediumCards extends React.Component{
         return(
             <div className="medium-cards-container">
                 <a href={this.props.post.link} target="_blank" rel="noopener noreferrer" className="card bg-dark text-white" >
-                    <img className="card-img" src={this.props.post.thumbnail} alt="medium post thumbnail"/>
-                    <div className="card-img-overlay" >
+                    <img className="card-img medium-card-image" src={this.props.post.thumbnail} alt="medium post thumbnail"/>
+                    <div className="" >
                     <h5 className="card-title card-text">{this.props.post.title}</h5>
                     <p className="card-text card-text">Posted on: {this.props.post.pubDate}</p>
                     </div>
