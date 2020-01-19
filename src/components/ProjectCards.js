8c5bd048bb99a565dfa51ccca1ec243afdffd405
Nick Boober <nboober@terpmail.umd.edu>
2020-01-19 13:31:24 -0500

    incorporated a sweetalert when selecting my projects to alert that it may take a few seconds to load

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Swal from 'sweetalert2'
 
 class ProjectCards extends React.Component{
+
+    alert = () => {
+        Swal.fire({
+            title: 'Opening Project in new Window',
+            text: "My project is opening. \n Please be patient, the frontend and backend of my projects are deployed on heroku and may take a few seconds to load.",
+            icon: 'info'
+          })
+    }
+
     render(){
         return(
-            <div className="project-cards-container">
+            <div className="project-cards-container" onClick={this.alert}>
                 <a href={this.props.project.deploy} target="_blank" rel="noopener noreferrer" className="card bg-dark text-white" >
                     <img className="card-img project-card-image" src={this.props.project.image} alt="project icon"/>
                     <div className="" >
