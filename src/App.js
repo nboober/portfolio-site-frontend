f4d548b9941b0a46f35f2b48b97717b95747e3ed
Nick Boober <nboober@terpmail.umd.edu>
2020-01-19 12:53:06 -0500

    fetched rojects to projects component and sorted them into correct state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 import './App.css';
 import AboutMe from './components/AboutMe'
 import NavBar from './components/NavBar'
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,43 @@
 import React from 'react';
 
 class Projects extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            reactProjects: [],
+            javascriptProjects: [],
+            rubyProjects: [],
+            javaProjects: []
+        }
+    }
+
+    componentDidMount = () => {
+        fetch('http://localhost:3000/projects')
+        .then(response => response.json())
+        .then(projects => {
+            projects.map((project)=>{
+                if(project.language.toLowerCase().includes("react")){
+
+                    this.setState({reactProjects: [...this.state.reactProjects, project]})
+    
+                }else if(project.language.toLowerCase().includes("javascript")){
+    
+                    this.setState({javascriptProjects: [...this.state.javascriptProjects, project]})
+    
+                }else if(project.language.toLowerCase().includes("ruby")){
+    
+                    this.setState({rubyProjects: [...this.state.rubyProjects, project]})
+    
+                }else if(project.language.toLowerCase().includes("javaback")){
+    
+                    this.setState({javaProjects: [...this.state.javaProjects, project]})
+    
+                }
+            })
+        })
+        
+    }
+
     render(){
         return(
             <div>
